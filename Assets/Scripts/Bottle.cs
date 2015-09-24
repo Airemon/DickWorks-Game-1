4a39ae96e21d21843611d45300bf84379d7e3bf2
Airemon <airemon12@gmail.com>
2015-09-23 21:48:26 -0700

    Justin - Changing movement to WADS, adding bottle assets that disappear on touching.

---
+++ Assets/Scripts/Bottle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bottle : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject gObj = other.gameObject;
+        AudioClip clip;
+        
+        if (gObj.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+	Animator anim;
+	
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKey(KeyCode.D))
+		{
+			transform.Translate (Vector2.right * speed);
+		}
+		
+		if(Input.GetKey(KeyCode.S))
+		{
+			transform.Translate (Vector2.down * speed);
+		}
+		
+		if(Input.GetKey(KeyCode.A))
+		{
+			transform.Translate (Vector2.left * speed);
+		}
+		
+		if(Input.GetKey(KeyCode.W))
+		{
+			transform.Translate (Vector2.up * speed);
+		}
+		
+		if( Input.GetMouseButtonDown( 0 ) )
+        {
+            anim.SetTrigger( "Attack" );
+        }
+	
+	}
+}
