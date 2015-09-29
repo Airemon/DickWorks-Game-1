1ae152c89e98a92e92d93a16a2e7bda3d87eb4de
Jeff Isaak <jisaak@gmail.com>
2015-09-28 19:25:19 -0700

    Key down code for collecting bottles.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,14 @@ public class Player : MonoBehaviour {
 public float speed;
 public bool hasBottle;
 
+	[SerializeField]
+	private float _delayInSeconds = 0.5f;
+	[SerializeField]
+	private bool _destroyOnlyOne = true;
+
+	private float _keyPressDuration = -1f;
+	private bool _bottleDestroyed = false;
+
     Animator anim;
 
     void Start ()
@@ -20,14 +28,58 @@ public bool hasBottle;
         {
             anim.SetTrigger( "Attack" );
         }
-		if(Input.GetKeyDown(KeyCode.E) && Bottle.WalkedOverObject)
-		{
-			Destroy(Bottle.WalkedOverObject);
+
+		// "Collect bottle" keyboard handling.
+		if (Input.GetKeyDown (KeyCode.E)) {
+			_bottleDestroyed = false;
+			_keyPressDuration = 0f;
 		}
+		if (Input.GetKey (KeyCode.E)) {
+			_keyPressDuration += Time.deltaTime;
+		}
+		if (Input.GetKeyUp (KeyCode.E)) {
+			_keyPressDuration = -1f;
+		}
+
     }
 	
 	void FixedUpdate() 
     {
 
 	}
+
+	// Need to have bottle collision checked in both onTriggerStay and onTriggerEnter!
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		attemptToDestroyBottle (other.gameObject);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		attemptToDestroyBottle (other.gameObject);
+	}
+
+	private void attemptToDestroyBottle(GameObject possibleBottle)
+	{
+		// If the key hasn't been down long enough, or if a bottle is
+		// already destroyed, do nothing.
+		if (_keyPressDuration < _delayInSeconds || _bottleDestroyed) {
+			return;
+		}
+
+		// If our collision object isn't actually a bottle, do nothing.
+		Bottle bottle = possibleBottle.GetComponent<Bottle> ();
+		if (bottle == null) {
+			return;
+		}
+
+		// Destroy the bottle.
+		Destroy(possibleBottle);
+		if( _destroyOnlyOne )
+		{
+			_bottleDestroyed = true;
+		}
+	}
+
 }
