be6d5628f60a9c5e45970fb2c74a1f32009aa3c3
Airemon <airemon12@gmail.com>
2015-10-04 18:10:40 -0700

    Justin - Attempted rotation of the player on key press.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,25 +16,29 @@ public class PlayerMovement : MonoBehaviour {
 	void Update () {
 	
 		_moveRate = speed * Time.deltaTime;
-		
+				
 		if(Input.GetKey(KeyCode.D))
 		{
 			transform.Translate (Vector2.right * speed);
+			transform.right = new Vector3(0f, -1f, 0f);
 		}
 		
 		if(Input.GetKey(KeyCode.S))
 		{
 			transform.Translate (Vector2.down * speed);
+			transform.up = new Vector3(0f, -1f, 0f);
 		}
 		
 		if(Input.GetKey(KeyCode.A))
 		{
 			transform.Translate (Vector2.left * speed);
+			transform.right = new Vector3(0f, 1f, 0f);
 		}
 		
 		if(Input.GetKey(KeyCode.W))
 		{
 			transform.Translate (Vector2.up * speed);
+			transform.up = new Vector3(0f, 1f, 0f);
 		}
 		
 		if( Input.GetMouseButtonDown( 0 ) )
