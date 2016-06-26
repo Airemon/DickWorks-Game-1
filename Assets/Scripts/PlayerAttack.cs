06ef414e9339aaf39d853442ebc24c2b61497004
Airemon <airemon12@gmail.com>
2016-06-25 18:01:07 -0700

    Bottle charges now work, like actually work, for reals.

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -2,25 +2,24 @@
 using System.Collections;
 
 public class PlayerAttack : MonoBehaviour {
-	
+
 	[SerializeField]
 	private Player _player;
-	
+
 	public int damageToGive;
-	
+
 	/*public float checkProjection(Vector2 a, Vector2 b){
 		return Vector2.Dot(a.normalized(), b.normalized());
 	}*/
 
 	void Start () {
-	
+		_player = GetComponent<Player>();
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
 		if(other.tag == "Foe"){
 			//Do damage to enemy and use a charge of the bottle weapon
 			other.GetComponent<EnemyHealth>().giveDamage(damageToGive);
-			
 			if(_player.bottleCharges > 0){
 				_player.bottleCharges --;
 			}
