4db57d1b1614ce0c55d1042748c1d4666630f085
Airemon <airemon12@gmail.com>
2016-06-27 23:22:36 -0700

    Justin - Working on the bottle throwing mechanic: Now able to spawn a clone of a bottle by holding down the 'action' key for 0.5 seconds, only functions if the player is carrying a bottle, and they can only spawn one before having to pick up a new bottle. Need to figure out why the clones are spawning so much smaller, and how to 'throw' the clones.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,63 +3,92 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-public float speed;
-public bool keyDown;
-public bool destroyOnlyOne;
-public bool bottleDestroyed = false;
+	public Animator anim;
 
-	public int bottleCharges = 0;
-	
-	[SerializeField]
-	private float _delayInSeconds = 0.5f;
+	public GameObject Beer_Bottle_1;
+	GameObject bottleInstance;
 
-	private float _keyPressDuration = -1f;
-	private bool Diagonal;
+	public Renderer Rend;
 
-    public Animator anim;
-	
+	public float speed;
+
+	public bool destroyOnlyOne;
+	public bool bottleDestroyed = false;
 	public bool hasBottle { get { return bottleCharges > 0; } }
 
-    void Start ()
-    {
-        anim = GetComponent<Animator>();
+	public int bottleCharges = 0;
 
-    }
+	private float bottleSpeed = 500f;
+	[SerializeField]
+	private float _actionKeyDelayInSeconds = 0.5f;
+	private float _actionKeyPressDuration = -1f;
 
-    void Update()
-    {
-        // "Collect bottle" keyboard handling.
-		//If the key is pressed this frame, set _bottleDestroyed
-		if (Input.GetKeyDown (KeyCode.E)){
-			bottleDestroyed = false;
-			_keyPressDuration = 0f;
-			keyDown = true;
+	private bool _triggerAction = false;
+	private bool _actionTriggered = false;
+	private bool _bottleThrown = false;
+	private bool Diagonal;
+
+	[SerializeField]
+	private KeyCode _actionKeyCode = KeyCode.E;
+
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+		Rend = GetComponent<Renderer> ();
+	}
+
+	void Update()
+	{
+		// Handling of the action key that is common to both throwing a bottle and picking up a bottle.
+
+		// If the key is pressed this frame, set the key press duration to 0 (start counting).
+		if (Input.GetKeyDown (_actionKeyCode)) {
+			_actionKeyPressDuration = 0f;
+			_actionTriggered = false;
 		}
-		//If the key is (still) held down, increment duration
-		if (Input.GetKey (KeyCode.E)){
-			_keyPressDuration += Time.deltaTime;
+
+		// If the key is (still) held down, increment duration (keep counting).
+		if (Input.GetKey (_actionKeyCode)) {
+			_actionKeyPressDuration += Time.deltaTime;
 		}
-		//If the key is released, reset the duration
-		if (Input.GetKeyUp (KeyCode.E)){
-			_keyPressDuration = -1f;
-			keyDown = false;
+
+		// If the key press duration is greater than the required delay, we are going to trigger the action (potentially subject to other things, of course).
+		if (_actionKeyPressDuration >= _actionKeyDelayInSeconds && !_actionTriggered) {
+			_triggerAction = true;
 		}
-				
-		/*if(hasBottle == true){
-			if(Input.GetKeyDown (KeyCode.E)){
-				_keyPressDuration = 0f;
-				keyDown = true;
+
+		// If the key is released, reset the duration and turn off the trigger action flag.
+		if (Input.GetKeyUp (_actionKeyCode)) {
+			_actionKeyPressDuration = -1f;
+			_triggerAction = false;
+		}
+
+		// "Collect bottle" specific stuff. Only applicable if the player does not have a bottle.
+		// Note that the triggering of the action occurs in the OnTriggerEnter and OnTriggerStay.
+		// The _triggerAction flag is checked & set there.
+		if (!hasBottle) {
+			// If the key is pressed this frame, set _bottleDestroyed to false.
+			if (Input.GetKeyDown (_actionKeyCode)) {
+				bottleDestroyed = false;
 			}
-			if(Input.GetKeyUp(KeyCode.E)){
-				_keyPressDuration += Time.deltaTime; 
-			}
-			if(Input.GetKeyUp(KeyCode.E)){
-				_keyPressDuration = -1f;
-				keyDown = false;
-			}
-			//Throw the Bottle!
-		}*/
-		
+		}
+
+		// "Throw bottle" specific stuff. Only applicable if the player has a bottle.
+		if (hasBottle && _triggerAction) {
+			// Throw bottle.
+			Debug.Log("THROW BOTTLE!");
+
+			// Put your bottle flying code here.
+			bottleInstance = Instantiate(Beer_Bottle_1, transform.position, Quaternion.Euler (new Vector3 (0, 0, 0))) as GameObject;
+			bottleInstance.SetActive (true);
+			//bottleInstance.velocity = transform.forward * bottleSpeed * Time.deltaTime;
+
+			bottleCharges = 0;
+			_triggerAction = false;
+			_actionTriggered = true;
+		}
+
+
 		//8-way movement animations
 		if(Input.GetKey(KeyCode.W) && Diagonal == false){
 			anim.SetBool("Up", true);
@@ -198,55 +227,55 @@ public bool bottleDestroyed = false;
 			anim.SetBool("RunUpLeft", false);
 		}
 		Diagonal = false;
-		
+
 		//Attack animation
 		if(hasBottle){
 			if(anim.GetBool("AttackDown")){
 				anim.SetBool("AttackDown", false);
 			}
-			if(Input.GetKeyUp(KeyCode.E) && anim.GetBool("Down") || Input.GetKeyUp(KeyCode.E) && anim.GetBool("RunDown")){
+			if(Input.GetKeyUp(_actionKeyCode) && anim.GetBool("Down") || Input.GetKeyUp(_actionKeyCode) && anim.GetBool("RunDown")){
 				anim.SetBool("AttackDown", true);
 			}
 			if(anim.GetBool("AttackDownLeft")){
 				anim.SetBool("AttackDownLeft", false);
 			}
-			if(Input.GetKeyUp(KeyCode.E) && anim.GetBool("DownLeft") || Input.GetKeyUp(KeyCode.E) && anim.GetBool("RunDownLeft")){
+			if(Input.GetKeyUp(_actionKeyCode) && anim.GetBool("DownLeft") || Input.GetKeyUp(_actionKeyCode) && anim.GetBool("RunDownLeft")){
 				anim.SetBool("AttackDownLeft", true);
 			}
 			if(anim.GetBool("AttackLeft")){
 				anim.SetBool("AttackLeft", false);
 			}
-			if(Input.GetKeyUp(KeyCode.E) && anim.GetBool("Left") || Input.GetKeyUp(KeyCode.E) && anim.GetBool("RunLeft")){
+			if(Input.GetKeyUp(_actionKeyCode) && anim.GetBool("Left") || Input.GetKeyUp(_actionKeyCode) && anim.GetBool("RunLeft")){
 				anim.SetBool("AttackLeft", true);
 			}
 			if(anim.GetBool("AttackUpLeft")){
 				anim.SetBool("AttackUpLeft", false);
 			}
-			if(Input.GetKeyUp(KeyCode.E) && anim.GetBool("UpLeft") || Input.GetKeyUp(KeyCode.E) && anim.GetBool("RunUpLeft")){
+			if(Input.GetKeyUp(_actionKeyCode) && anim.GetBool("UpLeft") || Input.GetKeyUp(_actionKeyCode) && anim.GetBool("RunUpLeft")){
 				anim.SetBool("AttackUpLeft", true);
 			}
 			if(anim.GetBool("AttackUp")){
 				anim.SetBool("AttackUp", false);
 			}
-			if(Input.GetKeyUp(KeyCode.E) && anim.GetBool("Up") || Input.GetKeyUp(KeyCode.E) && anim.GetBool("RunUp")){
+			if(Input.GetKeyUp(_actionKeyCode) && anim.GetBool("Up") || Input.GetKeyUp(_actionKeyCode) && anim.GetBool("RunUp")){
 				anim.SetBool("AttackUp", true);
 			}
 			if(anim.GetBool("AttackUpRight")){
 				anim.SetBool("AttackUpRight", false);
 			}
-			if(Input.GetKeyUp(KeyCode.E) && anim.GetBool("UpRight") || Input.GetKeyUp(KeyCode.E) && anim.GetBool("RunUpRight")){
+			if(Input.GetKeyUp(_actionKeyCode) && anim.GetBool("UpRight") || Input.GetKeyUp(_actionKeyCode) && anim.GetBool("RunUpRight")){
 				anim.SetBool("AttackUpRight", true);
 			}
 			if(anim.GetBool("AttackRight")){
 				anim.SetBool("AttackRight", false);
 			}
-			if(Input.GetKeyUp(KeyCode.E) && anim.GetBool("Right") || Input.GetKeyUp(KeyCode.E) && anim.GetBool("RunRight")){
+			if(Input.GetKeyUp(_actionKeyCode) && anim.GetBool("Right") || Input.GetKeyUp(_actionKeyCode) && anim.GetBool("RunRight")){
 				anim.SetBool("AttackRight", true);
 			}
 			if(anim.GetBool("AttackDownRight")){
 				anim.SetBool("AttackDownRight", false);
 			}
-			if(Input.GetKeyUp(KeyCode.E) && anim.GetBool("DownRight") || Input.GetKeyUp(KeyCode.E) && anim.GetBool("RunDownRight")){
+			if(Input.GetKeyUp(_actionKeyCode) && anim.GetBool("DownRight") || Input.GetKeyUp(_actionKeyCode) && anim.GetBool("RunDownRight")){
 				anim.SetBool("AttackDownRight", true);
 			}
 		}
@@ -254,10 +283,10 @@ public bool bottleDestroyed = false;
 			return;
 		}
 
-    }
-	
+	}
+
 	void FixedUpdate() 
-    {
+	{
 
 	}
 
@@ -276,7 +305,7 @@ public bool bottleDestroyed = false;
 	{
 		// If the key hasn't been down long enough, or if a bottle is
 		// already destroyed, do nothing.
-		if (_keyPressDuration < _delayInSeconds || bottleDestroyed) {
+		if (!_triggerAction || bottleDestroyed) {
 			return;
 		}
 
@@ -289,7 +318,7 @@ public bool bottleDestroyed = false;
 		// Destroy the bottle.
 		Destroy(possibleBottle);
 		destroyOnlyOne = true;
-		
+
 		if( destroyOnlyOne ){
 			bottleDestroyed = true;
 			bottleCharges += 3;
@@ -298,6 +327,10 @@ public bool bottleDestroyed = false;
 				bottleCharges = 3;
 			}
 		}
+
+		// Unset our _triggerAction and actionTriggered flags.
+		_triggerAction = false;
+		_actionTriggered = true;
 	}
 
 }
